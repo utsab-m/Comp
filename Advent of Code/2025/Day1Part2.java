01472039adcd530d0640200eecf3cc38490200db
Utsab Mondal <155183796+utsab-m@users.noreply.github.com>
2025-12-05 17:49:11 -0500

    Advent of Code Day 1 Part 1

---
+++ Advent of Code/2025/Day1Part2.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+public class Day1Part2 {
+    public static void main(String[] args) {
+        File file = new File("Day1Part1Input.txt");
+        String line;
+        int c = 0;
+        int pos = 50;
+
+        try {
+            Reader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+
+            while ((line = br.readLine()) != null) {
+                int turns = Integer.parseInt(line.substring(1));
+                if (line.charAt(0) == 'L') {
+                    turns *= -1;
+                } 
+                pos = (pos + turns) % 100;
+                if (pos == 0) c++;
+
+            }
+
+            br.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(c);
+    }
+}
