f8c87db03d0950cbb53212600bdac8466a302575
Utsab Mondal <155183796+utsab-m@users.noreply.github.com>
2025-12-06 21:01:28 -0500

    AOC Day 1 Part 2

--- Advent of Code/2025/Day1Part2.java
+++ Advent of Code/2025/Day1Part2.java
@@ -1,30 +1,46 @@
 import java.io.*;
 
 public class Day1Part2 {
+    public static int numZeros(long o, long n) {
+        int z = 0;
+
+        long smallZ = Math.min(o, n), bigZ = Math.max(o, n);
+
+        System.out.println(smallZ);
+        smallZ = (long)Math.floor((double)smallZ / 100);
+        System.out.println(smallZ);
+        System.out.println(bigZ);
+        bigZ = (long)Math.ceil((double)bigZ / 100);
+        System.out.println(bigZ);
+
+        z += bigZ - smallZ - 1;
+
+        return z;
+    }
     public static void main(String[] args) {
         File file = new File("Day1Input.txt");
         String line;
-        int c = 0;
-        int pos = 50;
+        long c = 0;
+        long pos = 50;
 
         try {
             Reader fr = new FileReader(file);
             BufferedReader br = new BufferedReader(fr);
 
             while ((line = br.readLine()) != null) {
-                int turns = Integer.parseInt(line.substring(1));
-                int crosses = (pos + turns) / 100;
+                long turns = Integer.parseInt(line.substring(1));
+                if (turns == 0) continue;
 
                 if (line.charAt(0) == 'L') {
                     turns *= -1;
                 } 
                 
-                int nextPos = pos + turns;
+                long nextPos = pos + turns;
+
+                c += numZeros(pos, nextPos);
+
                 pos = (nextPos) % 100;
-
-                
-
-                c += crosses;
+                if (pos == 0) c++;
             }
 
             br.close();
