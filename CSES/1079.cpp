f2435470a5f03035680ee61756e694e9d2a349d5
Utsab Mondal <utsabmondal19@gmail.com>
2026-05-25 21:03:23 -0400

    Binomial Coefficient

--- CSES/1079.cpp
+++ CSES/1079.cpp
@@ -1,42 +1,50 @@
 #include "bits/stdc++.h"
 using namespace std;
+using ll = long long;
 
-int binom(int a, int b) {
-    int mod = 1e9 + 7;
+const int MOD = 1e9+7, MAX = 1000005;
+ll factorial[MAX], invFactorial[MAX];
 
-    int num = 1;
+ll power(ll base, ll exp) {
+    base %= MOD;
 
-    vector<int> nums;
+    ll res = 1;
 
-    for (int i = 0; i < b; i++) {
-        nums.push_back(a-i);
+    while (exp > 0) {
+        if (exp % 2 == 1) (res *= base) %= MOD;
+        (base *= base) %= MOD;
+        exp /= 2;
     }
 
-    for (int i = 2; i <= b; i++) {
-        for (int j = 0; j < b; j++) {
-            if (nums[j] % i == 0) {
-                nums[j] /= i;
-                break;
-            }
-        }
-    }
+    return res;
+}
 
-    for (int n: nums) num = (num % mod) *  (n % mod) % mod;
-
-    return num;
+ll aCb(ll a, ll b) {
+    if (a <= 0 || b > a) return 0;
+    return (factorial[a] * invFactorial[b] % MOD * invFactorial[a-b] % MOD) % MOD;
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     
-    int x;
-    cin >> x;
+    int n;
+    cin >> n;
 
-    for (int i = 0; i < x; i++) {
+    factorial[0] = 1;
+    for (int i = 1; i < MAX; i++) {
+        (((factorial[i] = factorial[i-1] * i) %= MOD) += MOD) %= MOD;
+    }
+
+    invFactorial[MAX-1] = power(factorial[MAX-1], MOD-2);
+    for (int i = MAX-2; i >= 0; i--) {
+        invFactorial[i] = (invFactorial[i+1] * (i+1)) % MOD;
+    }
+
+    while (n--) {
         int a, b;
-        cin >> a;
-        cin >> b;
-        cout << binom(a, b) << "\n";
+        cin >> a >> b;
+
+        cout << aCb(a, b) << "\n";
     }
 }
