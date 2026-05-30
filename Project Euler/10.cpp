74f9da5c75ceacc57610136c67f1e682ebb5b42c
Utsab Mondal <utsabmondal19@gmail.com>
2026-05-30 19:07:59 -0400

    Largest Product in a Grid

--- Project Euler/10.cpp
+++ Project Euler/10.cpp
@@ -4,23 +4,19 @@ using ll = long long;
 
 const int MAX = 2000000;
 
-set<int> primes;
+vector<bool> primes(MAX+1, true);
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+
     ll sum = 0;
-
-    for (int i = 2; i < MAX; i++) {
-        primes.insert(i);
-    }
-
-    for (int i = 2; i < MAX; i++) {
-        if (primes.find(i) != primes.end()) {
+    
+    for (ll i = 2; i < MAX; i++) {
+        if (primes[i]) {
             sum += i;
-            for (int j = i; j < MAX; j += i) {
-                primes.erase(j);
+            for (ll j = i*i; j < MAX; j += i) {
+                primes[j] = false;
             }
         }
     }
---
+++ Project Euler/11.cpp
@@ -0,0 +1,61 @@
+#include "bits/stdc++.h"
+#include <fstream>
+using namespace std;
+using ll = long long;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    ifstream file("11.txt");
+
+    vector<vector<int>> grid(20, vector<int>(20));
+
+    for (int i = 0; i < 20; i++) {
+        for (int j = 0; j < 20; j++) {
+            string s;
+            file >> s;
+            grid[i][j] = stoi(s);
+        }
+    }
+
+    ll maxProduct = 1;
+
+    for (int i = 0; i < 20; i++) {
+        for (int j = 0; j < 20; j++) {
+            // get left, right, down, up, diag if possible
+            ll product = 1;
+            if (j - 3 >= 0) {
+                for (int k = 0; k <= 3; k++) product *= grid[i][j-k];
+                maxProduct = max(maxProduct, product);
+                product = 1;
+            }
+            if (j + 3 < 20) {
+                for (int k = 0; k <= 3; k++) product *= grid[i][j+k];
+                maxProduct = max(maxProduct, product);
+                product = 1;
+            }
+            if (i - 3 >= 0) {
+                for (int k = 0; k <= 3; k++) product *= grid[i-k][j];
+                maxProduct = max(maxProduct, product);
+                product = 1;
+            }
+            if (i + 3 < 20) {
+                for (int k = 0; k <= 3; k++) product *= grid[i+k][j];
+                maxProduct = max(maxProduct, product);
+                product = 1;
+            }
+            if (i + 3 < 20 && j + 3 < 20) {
+                for (int k = 0; k <= 3; k++) product *= grid[i+k][j+k];
+                maxProduct = max(maxProduct, product);
+                product = 1;
+            }
+            if (i + 3 < 20 && j - 3 >= 0) {
+                for (int k = 0; k <= 3; k++) product *= grid[i+k][j-k];
+                maxProduct = max(maxProduct, product);
+            }
+        }
+    }
+
+    cout << maxProduct << "\n";
+}
