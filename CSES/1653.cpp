002e70c3d80c894cbd6cab393a58978025c368ab
Utsab Mondal <utsabmondal19@gmail.com>
2026-07-04 17:08:29 -0400

    Elevator Rides

--- CSES/1653.cpp
+++ CSES/1653.cpp
@@ -18,6 +18,28 @@ int main() {
     dp[0] = {1, 0};
 
     for (int mask = 1; mask < (1 << n); ++mask) {
+        dp[mask] = {n+1, 0};
 
+        for (int i = 0; i < n; ++i) {
+            if (mask & (1 << i)) {
+                auto prev = dp[mask ^ (1 << i)];
+
+                int current_rides = prev.first, current_weight = prev.second;
+
+                // person fits in elevator
+                if (weights[i] + current_weight <= x) {
+                    current_weight += weights[i];
+                }
+                // person doesn't fit in elevator
+                else {
+                    current_rides++;
+                    current_weight = weights[i];
+                }
+
+                dp[mask] = min(dp[mask], {current_rides, current_weight});
+            }
+        }
     }
+
+    cout << dp[(1 << n) - 1].first << '\n';
 }
